50dc14820d1670a655ddb6ec005eb8337740112f
latesil <vihilantes@gmail.com>
2021-01-25 23:22:24 +0300

    reimplement: user folder entry checking

--- foldercleaner/user_folder.py
+++ foldercleaner/user_folder.py
@@ -24,6 +24,8 @@ class UserFoldersBoxRow(Handy.ActionRow):
     change_user_folder_button = Gtk.Template.Child()
     remove_user_folder_button = Gtk.Template.Child()
     user_folder_change_popover = Gtk.Template.Child()
+    file_folder_button_popover_entry = Gtk.Template.Child()
+    file_extension_button_popover_entry = Gtk.Template.Child()
 
     def __init__(self, extension=None, folder=None, *args, **kwargs):
         super().__init__(**kwargs)
@@ -33,8 +35,8 @@ class UserFoldersBoxRow(Handy.ActionRow):
 
         self.subtitle = extension if extension else constants['default_extension_name']
         self.title = folder if folder else constants['default_folder_name']
-        self.set_title(self.title)
-        self.set_subtitle(self.subtitle)
+        self.set_title(self.title)  # folder
+        self.set_subtitle(self.subtitle)  # extension
 
         # self.file_extension_button_label.props.label = self.extension
         # self.user_folder_button_label.props.label = self.folder
@@ -47,6 +49,16 @@ class UserFoldersBoxRow(Handy.ActionRow):
     @Gtk.Template.Callback()
     def on_remove_user_folder_button_clicked(self, btn):
         self.destroy()
+
+    @Gtk.Template.Callback()
+    def on_file_folder_button_popover_entry_changed(self, entry):
+        if self.check_entry(entry, self.non_letters_check):
+            self.set_title(entry.get_text())
+
+    @Gtk.Template.Callback()
+    def on_file_extension_button_popover_entry_changed(self, entry):
+        if self.check_entry(entry, self.non_letters_check):
+            self.set_subtitle(entry.get_text())
 
     # @Gtk.Template.Callback()
     # def on_close_user_folders_button_clicked(self, btn):
@@ -68,17 +80,17 @@ class UserFoldersBoxRow(Handy.ActionRow):
     #         self.folder = self.user_folder_button_label.props.label = entry.get_text().strip()
         
 
-    # def non_letters_check(self, text):
-    #     if text:
-    #         return True if re.match('^[A-Za-z0-9 _]+$', text) else False
+    def non_letters_check(self, text):
+        if text:
+            return True if re.match('^[A-Za-z0-9 _]+$', text) else False
 
-    # def check_entry(self, e, func):
-    #     text = e.props.text.strip()
-    #     e.props.secondary_icon_name = ''
-    #     if text:
-    #         if not func(text):
-    #             e.props.secondary_icon_name = 'dialog-warning-symbolic'
-    #             return False
-    #         else:
-    #             e.props.secondary_icon_name = ''
-    #             return True
+    def check_entry(self, e, func):
+        text = e.props.text.strip()
+        e.props.secondary_icon_name = ''
+        if text:
+            if not func(text):
+                e.props.secondary_icon_name = 'dialog-warning-symbolic'
+                return False
+            else:
+                e.props.secondary_icon_name = ''
+                return True
